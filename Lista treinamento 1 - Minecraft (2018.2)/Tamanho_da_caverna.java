9ec52149df4702fd2300776bceabaca51f11b64d
Guilherme Afonso <guilherme.asp@live.com>
2018-09-21 00:16:46 -0300

    Questão Tamanho da caverna da Lista de Treinamento

---
+++ Lista treinamento 1 - Minecraft (2018.2)/Tamanho_da_caverna.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class HuxleyCode {
+  public static void main(String args[]) {
+    	int l, a, b;
+		Scanner in = new Scanner(System.in);
+		l = in.nextInt();
+		a = in.nextInt();
+		b = l*l*a;
+		System.out.print(b);
+  }
+}
