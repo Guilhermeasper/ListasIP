9b5007abd5933bad60b56af649be6dd22ddd8b44
Guilherme Afonso <guilherme.asp@live.com>
2018-09-21 00:13:36 -0300

    Questão Nickname da Lista de Treinamento

---
+++ Lista treinamento 1 - Minecraft (2018.2)/Nickname.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class HuxleyCode {
+  public static void main(String args[]) {
+    	Scanner in = new Scanner(System.in);
+		String n = in.next();
+		String s = in.next();
+		String k = n+s;
+		System.out.print(k);
+  }
+}
