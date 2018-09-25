52e3cbad0cd8d06b54186d770dcacd7798a9e52f
Guilherme Afonso Silva Pereira <gasp@WINDOWS.CIN.UFPE.BR>
2018-09-25 17:07:11 -0300

    Lista 4

---
+++ Lista 4 - 2018.2 (Jogos Classicos)/L4Q4_Snake.java
@@ -0,0 +1,105 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class L4Q4_Snake {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        boolean perdeu = false;
+        int tamanhoMatriz, quantJogadas, i, j, posicaoX = 0, posicaoY = 0;
+        String jogada, auxiliar;
+        tamanhoMatriz = in.nextInt();
+        quantJogadas = in.nextInt();
+        String[][] mapa = new String[tamanhoMatriz][tamanhoMatriz];
+        for (i = 0; i < tamanhoMatriz; i++) {
+            for (j = 0; j < tamanhoMatriz; j++) {
+                mapa[i][j] = in.next();
+                if (mapa[i][j].equals("#")) {
+                    posicaoX = i;
+                    posicaoY = j;
+                }
+            }
+        }
+        while (quantJogadas > 0 && !perdeu) {
+            jogada = in.next();
+            if (jogada.equals("D")) {
+                if (posicaoX == tamanhoMatriz - 1) {
+                    if (mapa[0][posicaoY].equals("@")) {
+                        perdeu = true;
+                    } else {
+                        mapa[0][posicaoY] = "#";
+                        mapa[posicaoX][posicaoY] = "*";
+                    }
+                } else {
+                    if (mapa[posicaoX + 1][posicaoY].equals("@")) {
+                        perdeu = true;
+                    } else {
+                        mapa[posicaoX + 1][posicaoY] = "#";
+                        mapa[posicaoX][posicaoY] = "*";
+                    }
+                }
+            } else if (jogada.equals("E")) {
+                if (posicaoX == 0) {
+                    if (mapa[tamanhoMatriz - 1][posicaoY].equals("@")) {
+                        perdeu = true;
+                    } else {
+                        mapa[tamanhoMatriz - 1][posicaoY] = "#";
+                        mapa[posicaoX][posicaoY] = "*";
+                    }
+                } else {
+                    if (mapa[posicaoX - 1][posicaoY].equals("@")) {
+                        perdeu = true;
+                    } else {
+                        mapa[posicaoX - 1][posicaoY] = "#";
+                        mapa[posicaoX][posicaoY] = "*";
+                    }
+                }
+            } else if (jogada.equals("C")) {
+                if (posicaoY == 0) {
+                    if (mapa[posicaoX][tamanhoMatriz - 1].equals("@")) {
+                        perdeu = true;
+                    } else {
+                        mapa[posicaoX][tamanhoMatriz - 1] = "#";
+                        mapa[posicaoX][posicaoY] = "*";
+                    }
+                } else {
+                    if (mapa[posicaoX][posicaoY - 1].equals("@")) {
+                        perdeu = true;
+                    } else {
+                        mapa[posicaoX][posicaoY - 1] = "#";
+                        mapa[posicaoX][posicaoY] = "*";
+                    }
+                }
+            } else {
+                if (posicaoY == tamanhoMatriz - 1) {
+                    if (mapa[posicaoX][0].equals("@")) {
+                        perdeu = true;
+                    } else {
+                        mapa[posicaoX][0] = "#";
+                        mapa[posicaoX][posicaoY] = "*";
+                    }
+                } else {
+                    if (mapa[posicaoX][posicaoY + 1].equals("@")) {
+                        perdeu = true;
+                    } else {
+                        mapa[posicaoX][posicaoY + 1] = "#";
+                        mapa[posicaoX][posicaoY] = "*";
+                    }
+                }
+            }
+            quantJogadas -= 1;
+        }
+        for (i = 0; i < tamanhoMatriz; i++) {
+            for (j = 0; j < tamanhoMatriz; j++) {
+                System.out.print(mapa[i][j]);
+                if (j != tamanhoMatriz - 1) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println("");
+        }
+        if (perdeu) {
+            System.out.println("Game over.");
+        }
+    }
+}
