7b377433898ca3e0c0934b4859741043ead6ed1f
Guilherme Afonso <guilherme.asp@live.com>
2018-10-11 01:01:41 -0300

    Lista 5

--- Lista 5 - 2018.2 (High School Musical)/L5Q6_Together.java
+++ Lista 5 - 2018.2 (High School Musical)/L5Q6_Together.java
@@ -1,32 +1,39 @@
 import java.util.Scanner;
 
 public class L5Q6_Together {
+    private static String caminhos(boolean[][] matriz, String[] pessoas, int i, int j){
+        if(i >= matriz.length-1 || j >= matriz.length-1){
+            return pessoas[i];
+        }else if(matriz[i][j]){
+            return pessoas[i] + " " + caminhos(matriz, pessoas, i, j+1);
+        }else{
+            if(i==matriz.length-1){
+                i=0;
+                j++;
+            return caminhos(matriz, pessoas, i+1, j);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int quantPessoas = in.nextInt();
         int quantConexoes = in.nextInt();
         boolean[][] matrizAjdacencia = new boolean[quantPessoas][quantPessoas];
         String[] pessoas = new String[quantPessoas];
+        int[][] conexoes = new int[quantConexoes][2];
         for(int i = 0; i < pessoas.length; i++){
             pessoas[i] = in.next();
         }
-        for(int i = 0; i < matrizAjdacencia.length; i++){
-            for(int j = 0; j < matrizAjdacencia.length; j++){
-                if(pessoas[i].equals("Troy")){
-                    if(pessoas[j].equals("Sharpay") || pessoas[j].equals("Gabriela")){
-                        matrizAjdacencia[i][j] = true;
-                    }
-                }else if(pessoas[i].equals("Sharpay")){
-                    if(pessoas[j].equals("Gabriela")){
-                        matrizAjdacencia[i][j] = true;
-                    }
-                }else if(pessoas[i].equals("Gabriela")){
-                    if(pessoas[j].equals("Ryan")){
-                        matrizAjdacencia[i][j] = true;
-                    }
-                }
+        for(int i = 0; i < conexoes.length; i++){
+            for(int j = 0; j < 2; j++){
+                conexoes[i][j] = in.nextInt();
             }
+            matrizAjdacencia[conexoes[i][1]][conexoes[i][0]] = true;
         }
-        
+        String pessoaInicio = in.next();
+        String pessoaFim = in.next();
+
+        String teste = caminhos(matrizAjdacencia, pessoas, 0, 0);
+        System.out.println(teste);
     }
 }
