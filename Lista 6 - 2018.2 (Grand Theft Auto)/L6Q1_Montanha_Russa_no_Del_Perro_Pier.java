9230e92cf694dbf9788b2d7a3475482dd7f4c9d0
Guilherme Afonso <guilherme.asp@live.com>
2018-10-21 11:46:45 -0300

    Lista 6

--- Lista 6 - 2018.2 (Grand Theft Auto)/L6Q1_Montanha_Russa_no_Del_Perro_Pier.java
+++ Lista 6 - 2018.2 (Grand Theft Auto)/L6Q1_Montanha_Russa_no_Del_Perro_Pier.java
@@ -15,19 +15,22 @@ class Lista {
             this.proximo.inserir(numero);
         }
     }
-    public String toString() {
-        String resposta = "" + this.numeroDaSorte;
-        if (this.proximo != null) {
-            resposta = resposta + "," + this.proximo.toString();
+    public int[] procurar(int numero, int indice){
+        if(this.numeroDaSorte == numero){
+            return new int[]{this.numeroDaSorte, indice+1};
+        }else if(this.proximo != null){
+            return this.proximo.procurar(numero, indice+1);
+        }else{
+            return new int[]{-1, indice+1};
         }
-        return resposta;
     }
 }
 
 
 public class L6Q1_Montanha_Russa_no_Del_Perro_Pier {
     public static void main(String[] args) {
-        int bilhete = 0;
+        boolean achou = false;
+        int bilhete = 0, i;
         Lista numeros = new Lista();
         Scanner in = new Scanner(System.in);
         while (bilhete != -1){
@@ -35,8 +38,11 @@ public class L6Q1_Montanha_Russa_no_Del_Perro_Pier {
             numeros.inserir(bilhete);
         }
         int sorteado = in.nextInt();
-        for(int i = 0; i<numeros.toString().split(",").length; i++){
-            if(numeros.toString().split(",")[i].equals())
+        int[] achado = numeros.procurar(sorteado, 0);
+        if(achado[0] == -1){
+            System.out.println("Nenhum carrinho foi sorteado");
+        }else{
+            System.out.println("O bilhete sorteado e o " + sorteado + " e esta no carrinho " + achado[1]);
         }
     }
 }
