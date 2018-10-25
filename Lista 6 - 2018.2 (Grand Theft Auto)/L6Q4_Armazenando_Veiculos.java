0c5c4ca455937fcb7c8edae892d70f2c01ec7f24
Guilherme Afonso <guilherme.asp@live.com>
2018-10-25 01:36:44 -0300

    Lista 6

--- Lista 6 - 2018.2 (Grand Theft Auto)/L6Q4_Armazenando_Veiculos.java
+++ Lista 6 - 2018.2 (Grand Theft Auto)/L6Q4_Armazenando_Veiculos.java
@@ -3,25 +3,28 @@ import java.util.Scanner;
 class ListaVeiculos {
     private String veiculos;
     private ListaVeiculos proximo;
-    public ListaVeiculos () {
+
+    public ListaVeiculos() {
         veiculos = "";
         proximo = null;
     }
+
     public void inserir(String veiculos) {
         if (this.veiculos.equals("")) {
             this.veiculos = veiculos;
             this.proximo = new ListaVeiculos();
-        } else if(Integer.parseInt(this.veiculos) > Integer.parseInt(veiculos)){
+        } else if (Integer.parseInt(this.veiculos) > Integer.parseInt(veiculos)) {
             String aux = this.veiculos;
             this.veiculos = veiculos;
             this.proximo.inserir(aux);
-        }else{
+        } else {
             this.proximo.inserir(veiculos);
         }
     }
-    public void remover(String veiculos){
-        if(!this.veiculos.equals("")){
-            if(this.veiculos.equals(veiculos)){
+
+    public void remover(String veiculos) {
+        if (!this.veiculos.equals("")) {
+            if (this.veiculos.equals(veiculos)) {
                 this.veiculos = veiculos;
                 this.veiculos = this.proximo.veiculos;
                 this.proximo = this.proximo.proximo;
@@ -30,36 +33,39 @@ class ListaVeiculos {
             }
         }
     }
-    public String repetido(String veiculos){
-        if(this.veiculos.equals("")){
+
+    public String repetido(String veiculos) {
+        if (this.veiculos.equals("")) {
             return "";
-        }else{
-            if(this.veiculos.equals(this.proximo.busca(this.veiculos))){
+        } else {
+            if (this.veiculos.equals(this.proximo.busca(this.veiculos))) {
                 this.remover(this.veiculos);
                 return this.veiculos + " " + this.repetido(this.proximo.veiculos);
-            }else{
-                return this.proximo.repetido(this.proximo.veiculos) + "   ";
+            } else {
+                return this.proximo.repetido(this.proximo.veiculos);
             }
         }
     }
+
     public String toString() {
         String resposta = "" + this.veiculos;
         if (this.proximo != null) {
-            if(this.proximo.proximo != null)
+            if (this.proximo.proximo != null)
                 resposta = resposta + " " + this.proximo.toString();
             else
                 resposta = resposta + this.proximo.toString();
         }
         return resposta;
     }
-    public String busca(String veiculo){
-        if(!this.veiculos.equals("")){
-            if(this.veiculos.equals(veiculo)){
+
+    public String busca(String veiculo) {
+        if (!this.veiculos.equals("")) {
+            if (this.veiculos.equals(veiculo)) {
                 return veiculo;
-            }else{
+            } else {
                 return this.proximo.busca(veiculo);
             }
-        }else{
+        } else {
             return "";
         }
     }
@@ -75,10 +81,10 @@ public class L6Q4_Armazenando_Veiculos {
         ListaVeiculos tanque = new ListaVeiculos();
         ListaVeiculos submarino = new ListaVeiculos();
         String retorno;
-        while(in.hasNext()){
+        while (in.hasNext()) {
             String entrada = in.next();
             String veiculo = in.next();
-            if(entrada.equals("VERIFICAR")){
+            if (entrada.equals("VERIFICAR")) {
                 switch (veiculo.substring(0, 2)) {
                     case "67":
                         if (carro.busca(veiculo).equals(veiculo)) {
@@ -123,57 +129,57 @@ public class L6Q4_Armazenando_Veiculos {
                         }
                         break;
                 }
-            }else if(entrada.equals("IMPRIMIR")){
+            } else if (entrada.equals("IMPRIMIR")) {
                 switch (veiculo.substring(0, 2)) {
                     case "67":
                         retorno = carro.toString();
-                        if(retorno.equals("")){
+                        if (retorno.equals("")) {
                             System.out.println("A GARAGEM ESTA VAZIA :(");
-                        }else{
+                        } else {
                             System.out.println(retorno);
                         }
                         break;
                     case "65":
                         retorno = aviao.toString();
-                        if(retorno.equals("")){
+                        if (retorno.equals("")) {
                             System.out.println("A GARAGEM ESTA VAZIA :(");
-                        }else{
+                        } else {
                             System.out.println(retorno);
                         }
                         break;
                     case "77":
                         retorno = moto.toString();
-                        if(retorno.equals("")){
+                        if (retorno.equals("")) {
                             System.out.println("A GARAGEM ESTA VAZIA :(");
-                        }else{
+                        } else {
                             System.out.println(retorno);
                         }
                         break;
                     case "66":
                         retorno = bicicleta.toString();
-                        if(retorno.equals("")){
+                        if (retorno.equals("")) {
                             System.out.println("A GARAGEM ESTA VAZIA :(");
-                        }else{
+                        } else {
                             System.out.println(retorno);
                         }
                         break;
                     case "84":
                         retorno = tanque.toString();
-                        if(retorno.equals("")){
+                        if (retorno.equals("")) {
                             System.out.println("A GARAGEM ESTA VAZIA :(");
-                        }else{
+                        } else {
                             System.out.println(retorno);
                         }
                         break;
                     default:
                         retorno = submarino.toString();
-                        if(retorno.equals("")){
+                        if (retorno.equals("")) {
                             System.out.println("A GARAGEM ESTA VAZIA :(");
-                        }else{
+                        } else {
                             System.out.println(retorno);
                         }
                 }
-            }else if(entrada.equals("VEICULO!!!")){
+            } else if (entrada.equals("VEICULO!!!")) {
                 switch (veiculo.substring(0, 2)) {
                     case "67":
                         carro.inserir(veiculo);
@@ -197,34 +203,38 @@ public class L6Q4_Armazenando_Veiculos {
                         System.out.println("VERIFICAR TIPO DE VEICULO");
                         break;
                 }
-            }else{
+            } else {
                 String aux;
                 switch (veiculo.substring(0, 2)) {
                     case "67":
                         aux = carro.repetido("");
-                        if(!"".equals(aux))
-                            System.out.println(aux.substring(0,aux.length()-1));
+                        if (!"".equals(aux))
+                            System.out.println(aux.substring(0, aux.length() - 1));
                         break;
                     case "65":
                         aux = aviao.repetido("");
-                        if(!"".equals(aux))
-                            System.out.println(aux.substring(0,aux.length()-1));
+                        if (!"".equals(aux))
+                            System.out.println(aux.substring(0, aux.length() - 1));
                         break;
                     case "77":
                         aux = moto.repetido("");
-                        System.out.println(aux.substring(0,aux.length()-1));
+                        if (!"".equals(aux))
+                            System.out.println(aux.substring(0, aux.length() - 1));
                         break;
                     case "66":
                         aux = bicicleta.repetido("");
-                        System.out.println(aux.substring(0,aux.length()-1));
+                        if (!"".equals(aux))
+                            System.out.println(aux.substring(0, aux.length() - 1));
                         break;
                     case "84":
                         aux = tanque.repetido("");
-                        System.out.println(aux.substring(0,aux.length()-1));
+                        if (!"".equals(aux))
+                            System.out.println(aux.substring(0, aux.length() - 1));
                         break;
                     default:
                         aux = submarino.repetido("");
-                        System.out.println(aux.substring(0,aux.length()-1));
+                        if (!"".equals(aux))
+                            System.out.println(aux.substring(0, aux.length() - 1));
                 }
             }
         }
