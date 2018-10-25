181eadeed20ab0f8f8213284fc72ff8f8b685781
gasp <guilherme.asp@live.com>
2018-10-25 17:08:05 -0300

    Lista 6

--- Lista 6 - 2018.2 (Grand Theft Auto)/L6Q1_Montanha_Russa_no_Del_Perro_Pier.java
+++ Lista 6 - 2018.2 (Grand Theft Auto)/L6Q1_Montanha_Russa_no_Del_Perro_Pier.java
@@ -1,48 +1,51 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 class Lista {
     private int numeroDaSorte;
+    private int indice;
     private Lista proximo;
-    public Lista () {
-        numeroDaSorte = -1;
-        proximo = null;
+    public Lista (int indice) {
+        this.numeroDaSorte = -1;
+        this.proximo = null;
+        this.indice = indice;
     }
     public void inserir(int numero) {
         if (this.numeroDaSorte == -1) {
             this.numeroDaSorte = numero;
-            this.proximo = new Lista();
+            this.indice += 1;
+            this.proximo = new Lista(this.indice);
         } else {
             this.proximo.inserir(numero);
         }
     }
-    public int[] procurar(int numero, int indice){
+    public int procurar(int numero){
         if(this.numeroDaSorte == numero){
-            return new int[]{this.numeroDaSorte, indice+1};
+            return this.indice;
         }else if(this.proximo != null){
-            return this.proximo.procurar(numero, indice+1);
+            return this.proximo.procurar(numero);
         }else{
-            return new int[]{-1, indice+1};
+            return -1;
         }
     }
 }
-
 
 public class L6Q1_Montanha_Russa_no_Del_Perro_Pier {
     public static void main(String[] args) {
         boolean achou = false;
         int bilhete = 0, i;
-        Lista numeros = new Lista();
+        Lista numeros = new Lista(0);
         Scanner in = new Scanner(System.in);
         while (bilhete != -1){
             bilhete = in.nextInt();
             numeros.inserir(bilhete);
         }
         int sorteado = in.nextInt();
-        int[] achado = numeros.procurar(sorteado, 0);
-        if(achado[0] == -1){
+        int achado = numeros.procurar(sorteado);
+        if(achado == -1){
             System.out.println("Nenhum carrinho foi sorteado");
         }else{
-            System.out.println("O bilhete sorteado e o " + sorteado + " e esta no carrinho " + achado[1]);
+            System.out.println("O bilhete sorteado e o " + sorteado + " e esta no carrinho " + achado);
         }
     }
 }
